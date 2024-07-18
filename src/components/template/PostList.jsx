f073daf1938422cbb0549aed942a86fcd9e235d5
shayan <rasoolishayan18@gmail.com>
2024-07-18 15:25:42 +0330

    showing data from data base in the page

--- src/components/template/PostList.jsx
+++ src/components/template/PostList.jsx
@@ -1,12 +1,36 @@
 import { useQuery } from '@tanstack/react-query'
 import React from 'react'
 import { getPosts } from '../../../services/user'
+import Loader from "../module/Loader"
 
 function PostList() {
     const {data , isLoading } = useQuery(["my-post-list"] , getPosts)
     console.log(data);
   return (
-    <div>PostList</div>
+    <div>
+      {
+        isLoading ? <Loader /> : (
+          <>
+          <h3>اگهی های شما</h3>
+          {
+            data.data.posts.map(post => (
+              <div key={post._id}>
+                <img src={`http://localhost:3400/${post.images[0]}`}  />
+                <div>
+                  <p>{post.options.title}</p>
+                  <span>{post.options.content}</span>
+                </div>
+                <div>
+                  <p>{post.createdAt}</p>
+                  <span>{post.amount} تومان</span>
+                </div>
+              </div>
+            ))
+          }
+          </>
+        )
+      }
+    </div>
   )
 }
 
