56041b46074537a2eb3ae3ee8b57c58b4dc737f6
shayan <rasoolishayan18@gmail.com>
2024-07-18 17:18:28 +0330

    final commit

--- src/components/template/Main.jsx
+++ src/components/template/Main.jsx
@@ -1,7 +1,23 @@
+import {sp} from "../../../utils/Numbers.js"
+import styles from "./Main.module.css"
 
-function Main({post}) {
+function Main({posts}) {
+  console.log(posts);
   return (
-    <div>Main</div>
+    <div className={styles.container}>
+      {posts.data.posts.map((post) => (
+        <div key={post._id}>
+          <div className={styles.card}>
+            <p>{post.options.title}</p>
+            <div className={styles.info}>
+              <p>{sp(post.amount)} تومان</p>
+              <span>{post.options.city}</span>
+            </div>
+          </div>
+          <img src={`http://localhost:3400/${post.images[0]}`}/>
+        </div>
+      ))}
+    </div>
   )
 }
 
