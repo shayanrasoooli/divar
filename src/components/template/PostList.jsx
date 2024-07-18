0f7d9c74fd7b2faafbcb8a5e5b173df2536d6651
shayan <rasoolishayan18@gmail.com>
2024-07-18 15:48:39 +0330

    adding css to the page

--- src/components/template/PostList.jsx
+++ src/components/template/PostList.jsx
@@ -3,25 +3,26 @@ import React from 'react'
 import { getPosts } from '../../../services/user'
 import Loader from "../module/Loader"
 import { sp } from '../../../utils/Numbers'
+import styles from "./PostList.module.css"
 
 function PostList() {
     const {data , isLoading } = useQuery(["my-post-list"] , getPosts)
     console.log(data);
   return (
-    <div>
+    <div className={styles.list}>
       {
         isLoading ? <Loader /> : (
           <>
           <h3>اگهی های شما</h3>
           {
             data.data.posts.map(post => (
-              <div key={post._id}>
+              <div key={post._id} className={styles.post}>
                 <img src={`http://localhost:3400/${post.images[0]}`}  />
                 <div>
                   <p>{post.options.title}</p>
                   <span>{post.options.content}</span>
                 </div>
-                <div>
+                <div className={styles.price}>
                   <p>{new Date(post.createdAt).toLocaleDateString('fa-IR')}</p>
                   <span>{sp(post.amount)} تومان</span>
                 </div>
