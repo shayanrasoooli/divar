feeb2fe69b14183cd927a50ef7df610463fd363c
shayan <rasoolishayan18@gmail.com>
2024-07-18 16:21:04 +0330

    getting sidebar data from database and style it

--- src/components/template/SideBar.jsx
+++ src/components/template/SideBar.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import { useQuery } from '@tanstack/react-query'
+import {getCategory} from "../../../services/admin"
+import Loader from "../module/Loader"
+import styles from "./SideBar.module.css"
 
 function SideBar() {
+    const {data , isLoading} = useQuery(["get-categories" ], getCategory)
+    console.log(data);
   return (
-    <div>SideBar</div>
+    <div className={styles.sidebar}>
+        <h4>دسته ها</h4>
+        <ul>
+            {
+                isLoading ? <Loader /> : (
+
+                    data?.data.map((category) => (
+                        <li key={category._id}>
+                        <img src={`${category.icon}.svg`}/>
+                        <p>{category.name}</p>
+                    </li>
+                ))
+            )
+            }
+        </ul>
+    </div>
   )
 }
 
